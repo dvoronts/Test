56d94766122c4c3369a09a209b688ea89a5f8b4c
dvoronts <Comondante@gmail.com>
2013-09-04 17:59:54 +0300

    try rollback

--- Hello/src/com/hello/denis/MyServlet.java
+++ Hello/src/com/hello/denis/MyServlet.java
@@ -36,7 +36,7 @@ public class MyServlet extends HttpServlet {
         @Override
         public void run() {
             try {
-    			Thread.sleep(88000);
+    			Thread.sleep(999000);
                 asyncContext.getResponse().getWriter().println("hello " +  ++i + " ");
             } catch (Exception e) {
                 throw new RuntimeException(e);

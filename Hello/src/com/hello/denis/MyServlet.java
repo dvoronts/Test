eef54375bca70cea9b554c3ddf9cb9abb2895f7c
dvoronts <Comondante@gmail.com>
2013-09-04 16:36:52 +0300

    test

---
+++ Hello/src/com/hello/denis/MyServlet.java
@@ -0,0 +1,48 @@
+package com.hello.denis;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9122125709896866661L;
+	private int i;
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+        PrintWriter pw =  response.getWriter();
+		pw.write("Start ");
+		final AsyncContext asyncContext = request.startAsync(request, response);
+        asyncContext.getResponse().getWriter().println("async ");
+        asyncContext.start(new StockPriceRunner(asyncContext));
+
+        pw.write("Done ");
+	}
+
+	private class StockPriceRunner implements Runnable {
+        AsyncContext asyncContext;
+        
+        public StockPriceRunner(AsyncContext asyncContext) {
+            this.asyncContext = asyncContext;
+        }
+ 
+        @Override
+        public void run() {
+            try {
+    			Thread.sleep(3000);
+                asyncContext.getResponse().getWriter().println("hello " +  ++i + " ");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            } finally {
+                asyncContext.complete();
+            }
+        }
+    }
+}
